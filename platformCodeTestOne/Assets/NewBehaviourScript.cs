219268c6c3407e4468b891dd7d541ef9023fe927
Nick K <fozbstudios@gmail.com>
2018-03-03 06:29:07 -0600

    android project

--- platformCodeTestOne/Assets/NewBehaviourScript.cs
+++ platformCodeTestOne/Assets/NewBehaviourScript.cs
@@ -6,11 +6,24 @@ public class NewBehaviourScript : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+        
         Debug.Log("starting");
-	}
-	
-	// Update is called once per frame
-	void Update () {
+#if UNITY_EDITOR_WIN
+        Debug.Log("win1");
+        Debug.Log("win2");
+#endif
+#if UNITY_EDITOR_WIN
+        Debug.Log("a");
+#endif
+#if UNITY_EDITOR_OSX
+        Debug.Log("BBBBBBB");
+#endif
+        AndroidJavaClass pluginClass = new AndroidJavaClass("edu.cmu.pocketsphinx.demo.PocketSphinxActivity");
+        pluginClass.Call("onCreate", null);
+    }
+
+    // Update is called once per frame
+    void Update () {
 		
 	}
 }
