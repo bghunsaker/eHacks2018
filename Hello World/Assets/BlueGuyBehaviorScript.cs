5d66d20f57e9759deaeeb4815889c016fc369960
Jacob Bacon <jacobacon95@yahoo.com>
2018-03-03 06:17:22 -0600

    First Commit of Test File
    Three moving objects: rolling sphere, rotating platform, raising/lowering box.

---
+++ Hello World/Assets/BlueGuyBehaviorScript.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BlueGuyBehaviorScript : MonoBehaviour {
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        Debug.Log("Character Collision detected with " + col.gameObject.name);
+
+        if (col.gameObject.name =="dropBox1")
+        {
+            
+            Debug.Log("Dead");
+            
+                transform.Translate(Vector2.left);
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            
+            
+        } 
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(new Vector3(1, 0, 0));
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(new Vector3(-1, 0, 0));
+        }
+        if(Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.Space))
+        {
+            transform.Translate(new Vector3(0, 1, 0));
+        }
+
+    }
+
+    
+}
---
+++ Hello World/Assets/DroppingBoxBehavior.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DroppingBoxBehavior : MonoBehaviour {
+    public bool isUp = true;
+    int counter = 0;
+    // Use this for initialization
+    void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    //timescale rather than frames
+    private void FixedUpdate()
+    {
+        if(isUp == true) {
+          //  Debug.Log("Going Down");
+            transform.Translate(Vector2.down);
+        }
+        if(isUp == false)
+        {
+            transform.Translate(Vector2.up);
+            counter++;
+            if (counter >= 50)
+            {
+                isUp = true;
+            }
+        }
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+      
+        isUp = false;
+        counter = 0;
+      //  Debug.Log("Box Collision detected");
+    }
+}
