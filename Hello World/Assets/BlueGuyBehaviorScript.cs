d4cb0b83f6780d0da7055166868e182d906e19c7
Jacob Bacon <jacobacon95@yahoo.com>
2018-03-03 16:53:33 -0600

    integration with voice failed

---
+++ Hello World/Assets/BlueGuyBehaviorScript.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BlueGuyBehaviorScript : MonoBehaviour {
+    string command = "";
+    AndroidJavaClass jc = null;
+    AndroidJavaObject jo = null;
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        Debug.Log("Character Collision detected with " + col.gameObject.name);
+
+        if (col.gameObject.name =="dropBox1")
+        {
+            Debug.Log("Dead");
+                transform.Translate(Vector2.left);
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name); 
+        }
+        if (col.gameObject.name == "cannonBall")
+        {
+            Debug.Log("Dead");
+            transform.Translate(Vector2.left);
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+
+    //Use this for initialization
+
+   void Start () {
+        AndroidJavaClass jc = new AndroidJavaClass("edu.cmu.pocketsphinx.demo.GooseGameActivity");
+        AndroidJavaObject jo = jc.GetStatic<AndroidJavaObject>("instance");
+    }
+
+    // Update is called once per frame
+    void Update () {
+        command = jc.Call<string>("shareCommand");
+
+        //forward - crouch
+       // if (Input.GetKey(KeyCode.D))
+       if(command.Equals("crouch"))
+        {
+            transform.Translate(new Vector3(1, 0, 0));
+        }
+        //backward - shoot
+        //if (Input.GetKey(KeyCode.A))
+        if(command.Equals("shoot"))
+        {
+            transform.Translate(new Vector3(-1, 0, 0));
+        }
+        //jump
+        //if(Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.Space))
+        if(command.Equals("jump"))
+        {
+            transform.Translate(new Vector3(0, 1, 0));
+        }
+      //  if (Input.GetKey(KeyCode.P))
+       // {
+            //GameObject.Find("projectile").
+       // }
+
+    }
+
+    
+}
---
+++ Hello World/Assets/CannonBallBehavior.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CannonBallBehavior : MonoBehaviour {
+    public bool isUp = false;
+    int counter = 0;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void FixedUpdate()
+    {
+        
+        if (isUp == true)
+        {
+            
+            for (int i = 0; i < 80; i++)
+            {
+                transform.Translate(new Vector3(0, -1, 0));
+            }
+            isUp = false;
+            
+        }
+        if (isUp == false)
+        {
+            transform.Translate(Vector2.up);
+            counter++;
+            if (counter >= 80)
+            {
+                isUp = true;
+                counter = 0;
+            }
+        }
+
+    }
+}
